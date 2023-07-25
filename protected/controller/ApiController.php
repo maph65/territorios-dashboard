61ce0d5ea0c1497e97d8596ce02e963e659bc369
Miguel Pérez <miguel.perez@brainforth.com>
2023-07-25 10:41:27 -0600

    Base Datos

---
+++ protected/controller/ApiController.php
@@ -0,0 +1,62 @@
+<?php
+
+Doo::loadController('SecurityController');
+class MainController extends SecurityController {
+
+    public function login() {
+        session_start();
+        if(self::validarSesion()){
+            header('location:'.Doo::conf()->APP_URL.'home');
+        }else{
+            session_destroy();
+            $this->renderc('login');
+        }
+    }
+    
+    
+    public function home() {
+        session_start();
+        if(!self::validarSesion()){
+            header('location:'.Doo::conf()->APP_URL.'?error=2');
+            session_destroy();
+            die();
+        }
+        Doo::loadModel('CtUsuario'); 
+        $usuario = unserialize($_SESSION['usuario']);
+        $permisos = isset($usuario->permisos) ? unserialize($usuario->permisos) : array();
+        //print_r($permisos);
+        $home = NULL;
+        foreach ($permisos as $k => $p){
+            if($p){
+                $home = $k;
+                break;
+            }
+        }
+        if(!is_null($home)){
+            header('location:'.Doo::conf()->APP_URL.$home);
+        }else{
+            header('location:'.Doo::conf()->APP_URL.'logout');
+        }
+    }
+    
+    
+    public function connectAppInstall() {
+        //$this->renderc('apps/appConnect');
+        header('location:'.Doo::conf()->APP_URL.'appstore/');
+    }
+    
+    public function cotizadorAppInstall() {
+        //$this->renderc('apps/appCotizador');
+        header('location:'.Doo::conf()->APP_URL.'appstore/');
+    }
+
+    public function gen_model() {
+        Doo::loadCore('db/DooModelGen');
+        DooModelGen::genMySQL();
+    }
+    
+    
+
+}
+
+?>
