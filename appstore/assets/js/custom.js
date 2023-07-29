959141dcb67b7a4a6eaf525193f03c087acc3995
Miguel Pérez <miguel.perez@brainforth.com>
2023-07-29 11:57:20 -0600

    Removed unised files

--- appstore/assets/js/custom.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by marcelo.portugal on 10/4/15.
- */
-(function(){
-  $('.bs-component [data-toggle="popover"]').popover();
-  $('.bs-component [data-toggle="tooltip"]').tooltip();
-})();
